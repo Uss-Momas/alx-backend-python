7d59df43d28bbba9d9bcc74a84ad580ee9c51ce3
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-09 12:12:35 +0200

    creating sort function to order the delay list in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """module with multiple concurrent coroutines"""
-from typing import List
+from typing import Any, List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -12,5 +12,26 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(0, n):
         value = await wait_random(max_delay)
         delays_list.append(value)
-    delays_list.sort()
+    quick_sort(delays_list, 0, len(delays_list) - 1)
     return delays_list
+
+
+def quick_sort(target_list, low: Any, high: Any) -> list:
+    """using the quicksort algorithm to sort a list"""
+    if low < high:
+        pi = partition(target_list, low, high)
+        quick_sort(target_list, low, pi - 1)
+        quick_sort(target_list, pi + 1, high)
+
+
+def partition(array, low, high):
+    """find partition position"""
+    pivot = array[high]
+
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1

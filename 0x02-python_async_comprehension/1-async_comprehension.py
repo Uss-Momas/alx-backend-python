47bf4db5f4c81d2633a1894145a707f9eff3c0fd
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-10 19:06:02 +0200

    fix check error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,7 @@
 """Async comprehension module"""
 from typing import List
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:

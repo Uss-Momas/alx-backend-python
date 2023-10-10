b7c600b703133127a56f9ad08463bd2f40817958
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-10 17:22:03 +0200

    Add typing for file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list:
     """async comprehension"""
     results = [i async for i in async_generator()]
     return results

1f5ce26494aa9b6026a5320f3c8a5e50fa743cae
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-10 18:56:43 +0200

    fix check error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """Async generator module"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """async generator  of 10 numbers"""
-    for i in range(0, 10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)

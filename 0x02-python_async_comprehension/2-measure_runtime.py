4eb37c1b7760889632aa4b3907850fe5189e156a
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-10 17:40:04 +0200

    measuring the runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""measure runtime module"""
+import time
+import asyncio
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    """measure runtime function"""
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(), async_comprehension(), async_comprehension()
+    )
+    return time.perf_counter() - start_time

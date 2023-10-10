158585c57bbd66d485405283a877009f0bb010a0
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-10 17:24:26 +0200

    typing for async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async generator module"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """async generator  of 10 numbers"""
     for i in range(0, 10):
         await asyncio.sleep(1)

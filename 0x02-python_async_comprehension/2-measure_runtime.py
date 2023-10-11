499a7352ea4c6db276cf3ec8791ce00960b23ed0
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-11 13:52:20 +0200

    Fix: asyncio.gather

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,10 +9,6 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 async def measure_runtime() -> float:
     """measure runtime function"""
     start_time = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension(),)
     return time.perf_counter() - start_time
